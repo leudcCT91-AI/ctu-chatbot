ac8b4d94dba0aac648417c9cfe8cf09c996724a3
leducCT91-AI <leduc.ct91@gmail.com>
2026-03-06 13:34:03 +0700

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from chatbot import get_response
+
+st.title("🎓 CTU Chatbot")
+
+user_input = st.text_input("Nhập câu hỏi")
+
+if user_input:
+    answer, suggestions = get_response(user_input)
+
+    st.write("Bot:", answer)
+
+    if suggestions:
+        st.write("Gợi ý:")
+        for s in suggestions:
+            st.write("-", s)
