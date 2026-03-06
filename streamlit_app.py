7c7b4d9f3c31939f0890db8147a4b67d70cd5402
leducCT91-AI <leduc.ct91@gmail.com>
2026-03-06 14:55:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.title("🎓 CTU Chatbot")
 user_input = st.text_input("Nhập câu hỏi")
 
 if user_input:
-    answer, suggestions = get_response(user_input)
+    answer, suggestions = get_response(user_input, df, vectorizer, faq_matrix)
 
     st.write("Bot:", answer)
 
